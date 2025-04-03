88e48c33870ba67119ccce9dca5c489fd252728a
2048khz-gachi-rmx <44749188+2048khz-gachi-rmx@users.noreply.github.com>
2025-04-03 17:55:15 +0700

    Update ChannelFlagCommandsTests.cs

--- Tests/Tests/Commands/ChannelFlagCommandsTests.cs
+++ Tests/Tests/Commands/ChannelFlagCommandsTests.cs
@@ -1,6 +1,5 @@
-﻿using Dotto.Application.Modules.ChannelFlags;
-using MediatR;
-using NetCord;
+﻿using Dotto.Application.Entities;
+using Dotto.Application.Modules.ChannelFlags;
 using NetCord.Rest;
 using Shouldly;
 
@@ -31,6 +30,32 @@ public class ChannelFlagCommandsTests : TestFixtureBase
     }
     
     [Test]
+    public async Task ShouldReplyAddLimitReached()
+    {
+        // Arrange
+        var now = TestDateTimeProvider.SetNow();
+
+        var flag = await ChannelFlagBuilder
+            .WithChannelId(123)
+            .WithFlags(Enumerable.Repeat("flag", Constants.ChannelFlags.MaxFlagsInChannel).ToList())
+            .WithUpdatedOn(now.AddDays(-1))
+            .GetAsync();
+        
+        // Send
+        NewScope();
+        var shouldThrow = async () => await Mediator.Send(new AddFlagRequest<InteractionMessageProperties>()
+        {
+            ChannelId = 123,
+            FlagName = "newflag"
+        });
+        
+        // Assert
+        NewScope();
+        (await shouldThrow.ShouldThrowAsync<ArgumentOutOfRangeException>())
+            .Message.ShouldStartWith("Channel reached the flag limit");
+    }
+    
+    [Test]
     public async Task ShouldRemoveFlag()
     {
         // Arrange
@@ -50,5 +75,46 @@ public class ChannelFlagCommandsTests : TestFixtureBase
         msg.Content.ShouldStartWith("Flag removed!");
         msg.Content!.ShouldContain("keepFlag");
         msg.Content!.ShouldNotContain("removeFlag");
+    }
+
+    [Test]
+    public async Task ShouldRemoveNonExistentFlag()
+    {
+        // Arrange
+        var flag = await ChannelFlagBuilder
+            .WithChannelId(123)
+            .WithFlags(["keepFlag"])
+            .GetAsync();
+        
+        // Send
+        var msg = await Mediator.Send(new RemoveFlagRequest<InteractionMessageProperties>()
+        {
+            ChannelId = 123,
+            FlagName = "nonExistentFlag"
+        });
+        
+        // Assert
+        msg.Content.ShouldStartWith("Channel didn't have flag \"nonExistentFlag\".");
+    }
+    
+    [Test]
+    public async Task ShouldListFlags()
+    {
+        // Arrange
+        var flag = await ChannelFlagBuilder
+            .WithChannelId(123)
+            .WithFlags(["flag1", "flag2"])
+            .GetAsync();
+
+        // Send
+        var msg = await Mediator.Send(new ListFlagsRequest<InteractionMessageProperties>()
+        {
+            ChannelId = 123
+        });
+
+        // Assert
+        msg.Content.ShouldStartWith("Channel has the following flags:");
+        msg.Content!.ShouldContain("flag1");
+        msg.Content!.ShouldContain("flag2");
     }
 }
